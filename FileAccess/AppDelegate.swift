13e6ef8ff8172fce67fe2ca79466fd3760422cdb
YangWei <84344@loncinindustries.com>
2023-08-02 13:46:40 +0800

    Initial Commit

---
+++ FileAccess/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  FileAccess
+//
+//  Created by long on 2023/8/2.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ FileAccess/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FileAccess
+//
+//  Created by long on 2023/8/2.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
